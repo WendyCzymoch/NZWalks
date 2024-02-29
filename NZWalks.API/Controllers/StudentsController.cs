9c93a59c5848421cd584d6b635a856e02c075f0c
VUDS\czymocw <wendy.czymoch@vanderbilt.edu>
2024-02-29 13:56:51 -0600

    Add project files.

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+
+    // https://localhost:portnumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            var students = new[] { "Adam", "Beth", "Charlie", "Diana", "Eli" };
+
+            return Ok(students);
+        }
+    }
+}
