f3a6d98e9b31ed921dd6ab883e03d1dad2a5f38c
VUDS\czymocw <wendy.czymoch@vanderbilt.edu>
2024-02-29 14:09:51 -0600

    New domain models

---
+++ NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,16 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double? LengthInKilometers { get; set; }
+        public Guid DifficultyId { get; set; }
+        public Guid RegionId { get; set; }
+
+        // navigation properties
+        public Difficulty Difficulty { get; set; }
+        public Region Region { get; set; }
+    }
+}
